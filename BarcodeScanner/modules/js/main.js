c56ae7e103a0fd34fc5139c248fefdeb77a1f1d9
KonyDev <platform_integration_team@kony.com>
2014-05-13 17:31:50 +0530

    first commit

---
+++ BarcodeScanner/modules/js/main.js
@@ -0,0 +1,74 @@
+function scanVINBarcode()
+{
+	PlatformName=kony.os.deviceInfo().name
+	kony.print("###################"+PlatformName);
+	if(kony.string.startsWith(PlatformName, "android", true) == true)
+	{		//frmScanned.show();
+		jsBarCodeAndroid();
+	}else if(kony.string.startsWith(PlatformName, "iphone", true) == true||kony.string.startsWith(PlatformName, "ipad", true) == true)
+	{
+		jsBarCodeiPhone();
+	}
+	else{
+		kony.print("This platform is not supported");
+	}
+}
+
+/** 
+ * 
+ * ============== Android Barcode Scanner ===========================
+ * 
+ */
+ 
+function jsBarCodeAndroid(  ){
+	scanbarcodeffi.getBarCodeAndroid(onBarcodeScan);
+}
+
+function onBarcodeScan(a, b){
+	kony.print("\n\n:--in onBarcodeScan-->\n");
+	//kony.print("value of scanned data-----"+b);
+	//kony.print("a=" + a);
+	//kony.print("b=" + b);
+	kony.print("value of scanned data for Android-----"+b);
+	gScannedText = b;
+	if (gScannedText == "" || gScannedText == null)
+	{
+		var infoAlert =  kony.ui.Alert({message:"Scanning Failed",alertType: constants.ALERT_TYPE_INFO,alertTitle: "CUB",yesLabel:"OK",noLabel: "Cancel", alertHandler: null},{});
+	}
+	else{
+	
+		kony.print("\n\n:--in onBarcodeScan2-->\n");
+		try{
+  				kony.timer.schedule("timerid", executeTimer, 1, false);
+  			}
+  			catch(err){
+     			kony.print("Timer already exists!!");
+  			}
+		
+		
+	}
+}
+function executeTimer()
+{
+	frmScndCntnt.lblScannedText.text="The scanned text is - " + gScannedText;
+	frmScndCntnt.show();
+}
+
+/**
+ * 
+ * ============================= IOS Barcode Scanner =================================
+ * 
+ */
+ 
+function jsBarCodeiPhone(dummy){
+	gScannedText = scanbarcodeffi.getBarCodeiPhone();
+	kony.print("gScannedText..........."+gScannedText);
+	if(gScannedText == "" || gScannedText == null){
+    	alert("Scanning failed, please scan again.")
+    	return false;
+    }else{
+    	kony.print("The text of the scanned barcode is: \n"+ gScannedText);
+		frmScndCntnt.lblScannedText.text = "The scanned text is - " + gScannedText;
+		frmScndCntnt.show();
+	}
+}
